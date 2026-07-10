ba9afc0b165e68a00525b0c50334feecdcb87060
Gerard Lutterop <gerard.lutterop@bizservices.nl>
2026-07-10 14:34:43 +0200

    debugging command executes only commands from debug module

--- providers/debug.py
+++ providers/debug.py
@@ -2,6 +2,7 @@
 Sitekick server.
 """
 import json
+import re
 from urllib.parse import urlencode
 from urllib.request import Request, urlopen
 
@@ -20,25 +21,21 @@ def is_server_type():
 def get_domains():
     """Get the intended info to retrieve. The command is retrieved from the Sitekick service, is retrieved according
     to the cron schedule, default every 5 minutes so commands can be changed and the result can be retrieved quite fast."""
-
-    result = [["plesk", "version"],
-            ["plesk", "bin", "site", "--list"],
-            ["plesk", "bin", "domain", "--info", "sitekick.eu"],
-            ["plesk", "db", "-sNe",
-             "SELECT d.name, h.php_handler_id FROM domains d JOIN hosting h ON h.dom_id=d.id WHERE d.name='sitekick.eu'"],
-            ["echo", hostname]]
     params = {'hostname': hostname or ip_address or mac_address}
     sitekick_url = SITEKICK_DEBUG_URL + '?' + urlencode(params)
     req = Request(sitekick_url, method='GET')
     # Get the list of commands, which is in the key 'command' in the json root of the request
     response = urlopen(req)
     data = json.loads(response.read())
-    result = data.get('commands', [])
-    return [repr(item) for item in result]
+    total_commands = []
+    for regex, commands in data.iteritems():
+        if any(re.fullmatch(regex, identifier, re.I) for identifier in (hostname, ip_address, mac_address)):
+            total_commands.extend(commands)
+    return [json.dumps(item) for item in total_commands]
 
 
 def get_domain_info(domain):
     """The domain is not a string, but a command line request, as a list."""
-    command = eval(domain)
+    command = json.loads(domain)
     result = cli(command, include_stderr=True)
     return {'output': result}
