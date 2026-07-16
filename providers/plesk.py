926bafd5682a94f5d341d075355bc3a4e5663011
Gerard Lutterop <gerard.lutterop@bizservices.nl>
2026-07-16 11:36:41 +0200

    Get plesk location from OS or config

--- providers/plesk.py
+++ providers/plesk.py
@@ -18,10 +18,6 @@ VERSION = '260712'
 
 plesk = cli(['which', 'plesk']).strip()
 print(f"=======++++++++======++++++ which plesk: {plesk}")
-print(f"=======++++++++======++++++ which plesk: {plesk}")
-print(f"=======++++++++======++++++ which plesk: {plesk}")
-print(f"=======++++++++======++++++ which plesk: {plesk}")
-print(f"=======++++++++======++++++ which plesk: {plesk}")
 if not plesk or ' ' in plesk:  # When no result, or some error which always contains at least one space.
     print(f"======= plesk not found, using config.PLESK_BINARY: {config.PLESK_BINARY}")
     plesk = config.PLESK_BINARY
